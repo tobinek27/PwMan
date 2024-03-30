87323a56d1d3bb626565a208839ca460a4db937b
tobinek27 <tobias.haltmar@gmail.com>
2024-03-30 15:09:46 +0100

    add password generating function

--- PwMan/Program.cs
+++ PwMan/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace PwMan;
 using System;
 using System.IO;
+using System.Text;
 
 class Program
 {
@@ -12,13 +13,13 @@ class Program
         Console.WriteLine(newUser.Password);
         
         // Check if the directory doesn't exist
-        string currentPath = Directory.GetCurrentDirectory();
-        string folderPath = $"{currentPath}/user_files/";
-        Console.WriteLine(currentPath);
-        Console.WriteLine(folderPath);
-
-        bool test = newUser.HasFile();
-        Console.WriteLine(test);
+        //string currentPath = Directory.GetCurrentDirectory();
+        //string folderPath = $"{currentPath}/user_files/";
+        //Console.WriteLine(currentPath);
+        //Console.WriteLine(folderPath);
+        
+        //bool test = newUser.HasFile();
+        //Console.WriteLine(test);
         //Console.WriteLine(newUser.CreateFile());
         /*if (!Directory.Exists(folderPath))
         {
@@ -30,5 +31,32 @@ class Program
         {
           Console.WriteLine("Folder already exists.");
         }*/
+        string testingPassword01 = GeneratePassword(10);
+        string testingPassword64 = GeneratePassword();
+        Console.WriteLine(testingPassword01);
+        Console.WriteLine(testingPassword64);
+        Console.WriteLine("please make a choice");
+        int userInput = Convert.ToInt32(Console.ReadLine());
+        switch (userInput)
+        {
+            case 1:
+                Console.WriteLine("neco");
+                break;
+        }
+    }
+
+    private static string GeneratePassword(int passwordLength = 64)
+    {
+        // const validCharacters contains all characters that could be used in the password generation process
+        const string validCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+        StringBuilder password = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < passwordLength; i++)
+        {
+            password.Append(validCharacters[random.Next(validCharacters.Length)]);
+        }
+
+        return password.ToString();
     }
 }
