32baf5a0795945008a65156502e5050196fd7909
tobinek27 <tobias.haltmar@gmail.com>
2024-04-23 14:04:51 +0200

    code improvements

--- PwMan/Program.cs
+++ PwMan/Program.cs
@@ -16,49 +16,11 @@ class Program
         switch (userInput)
         {
             case not null when (userInput != "register" && userInput.Length > 2 && userInput.Length < 17):
-                userInput = userInput.Trim();
-                User user1 = new User(userInput);
-                if (user1.HasLoginFile())
-                {
-                    Console.WriteLine($"Input user password for {userInput}: ");
-                    string passwordInput = Console.ReadLine();
-                    if (String.IsNullOrEmpty(passwordInput) || passwordInput.Length < 1)
-                    {
-                        Console.WriteLine("password is either null, or too short!\r\nterminating");
-                        break;
-                    }
-        
-                    // Retrieve the stored password information from the JSON file
-                    string filePath = $"{Directory.GetCurrentDirectory()}/user_logins/{userInput}.json";
-                    string json1 = File.ReadAllText(filePath);
-                    HashSalt storedHashSalt = HashSalt.FromJson(json1);
-        
-                    // Validate the entered password against the stored hash and salt
-                    bool isValidPassword = HashSalt.VerifyPassword(passwordInput, storedHashSalt.Hash, storedHashSalt.Salt);
-        
-                    // Output the login status
-                    Console.WriteLine($"user login status: {isValidPassword}");
-
-                    if (isValidPassword)
-                    {
-                        user1.LoggedIn = true;
-                        Console.WriteLine($"user '{user1.LoggedIn}' logged in successfully");
-                    }
-                    else
-                    {
-                        user1.LoggedIn = false;
-                        Console.WriteLine($"invalid password {passwordInput} for user '{userInput}'");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"user '{userInput}' does not have a profile registered");
-                    Console.WriteLine("shutting down");
-                    Environment.Exit(1);
-                }
+                HandleLogin(userInput);
                 break;
             case "register": // sign up a new account
-                Console.WriteLine("input a username to register:");
+                HandleRegistration();
+                /*Console.WriteLine("input a username to register:");
                 string usernameToRegister = Console.ReadLine();
                 if (User.HasLoginFile(usernameToRegister))
                 {
@@ -73,7 +35,7 @@ class Program
 
                 // Create login file for the new user
                 User.CreateLoginFile(usernameToRegister, inputPassword, hashSalt1.Password, hashSalt1.Salt);
-                Console.WriteLine($"User '{usernameToRegister}' registered successfully.");
+                Console.WriteLine($"User '{usernameToRegister}' registered successfully.");*/
                 break;
             default:
                 Console.WriteLine("invalid command input, terminating");
@@ -81,5 +43,68 @@ class Program
                 break;
         }
     }
-    
+
+    static void HandleLogin(string usernameInput)
+    {
+        string userInput = usernameInput.Trim();
+        User user1 = new User(userInput);
+        if (user1.HasLoginFile())
+        {
+            Console.WriteLine($"Input user password for {userInput}: ");
+            string passwordInput = Console.ReadLine();
+            if (String.IsNullOrEmpty(passwordInput) || passwordInput.Length < 1)
+            {
+                Console.WriteLine("password is either null, or too short!\r\nterminating");
+                Environment.Exit(1);
+            }
+        
+            // Retrieve the stored password information from the JSON file
+            string filePath = $"{Directory.GetCurrentDirectory()}/user_logins/{userInput}.json";
+            string json1 = File.ReadAllText(filePath);
+            HashSalt storedHashSalt = HashSalt.FromJson(json1);
+        
+            // Validate the entered password against the stored hash and salt
+            bool isValidPassword = HashSalt.VerifyPassword(passwordInput, storedHashSalt.Hash, storedHashSalt.Salt);
+        
+            // Output the login status
+            Console.WriteLine($"user {user1.Username} login status: {isValidPassword}");
+
+            if (isValidPassword)
+            {
+                user1.LoggedIn = true;
+                Console.WriteLine($"user '{user1.Username}' logged in successfully");
+            }
+            else
+            {
+                user1.LoggedIn = false;
+                Console.WriteLine($"invalid password {passwordInput} for user '{userInput}'");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"user '{userInput}' does not have a profile registered");
+            Console.WriteLine("shutting down");
+            Environment.Exit(1);
+        }
+    }
+
+    static void HandleRegistration()
+    {
+        Console.WriteLine("input a username to register:");
+        string usernameToRegister = Console.ReadLine();
+        if (User.HasLoginFile(usernameToRegister))
+        {
+            Console.WriteLine($"Sorry, but the username '{usernameToRegister}' is already taken.\r\nShutting down.");
+            Environment.Exit(1);
+        }
+        Console.WriteLine($"Input a password to be used for {usernameToRegister}:");
+        string inputPassword = Console.ReadLine();
+
+        int saltSize1 = 16; // Choose your salt size
+        HashSalt hashSalt1 = HashSalt.GenerateSaltedHash(saltSize1, inputPassword);
+
+        // Create login file for the new user
+        User.CreateLoginFile(usernameToRegister, inputPassword, hashSalt1.Password, hashSalt1.Salt);
+        Console.WriteLine($"User '{usernameToRegister}' registered successfully.");
+    }
 }
